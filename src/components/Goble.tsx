c4f850a0bbd8e4953e647b8fdc2a1f203e726a93
jorge luiz <jojoluiz42@gmail.com>
2024-05-25 11:43:57 -0300

    fix: Globe responsible mobile

--- src/components/Goble.tsx
+++ src/components/Goble.tsx
@@ -3,9 +3,12 @@ import React from "react";
 import { motion } from "framer-motion";
 import dynamic from "next/dynamic";
 
-const World = dynamic(() => import("@/components/ui/globle").then((m) => m.World), {
-  ssr: false,
-});
+const World = dynamic(
+  () => import("@/components/ui/globle").then((m) => m.World),
+  {
+    ssr: false,
+  }
+);
 
 export function GlobeDemo() {
   const globeConfig = {
@@ -395,34 +398,36 @@ export function GlobeDemo() {
   ];
 
   return (
-    <div className="flex flex-row items-center justify-center py-20 h-screen md:h-auto dark:bg-black bg-white relative w-full">
-      <div className="max-w-7xl mx-auto w-full relative overflow-hidden h-full md:h-[40rem] px-4">
-        <motion.div
-          initial={{
-            opacity: 0,
-            y: 20,
-          }}
-          animate={{
-            opacity: 1,
-            y: 0,
-          }}
-          transition={{
-            duration: 1,
-          }}    
-          className="div"
-        >
-          <h2 className="text-center text-xl md:text-4xl font-bold text-black ">
-          E disse-lhes: "Vão pelo mundo todo e preguem o evangelho a todas as pessoas". <br /> Marcos 15:16
-          </h2>
-          <p className="text-center text-base  md:text-lg font-normal text-neutral-700 mt-2 mx-auto">
-          Thanks Aceternity UI for this beautiful component! God bless you all ✨
-          </p>
-        </motion.div>
-        <div className="absolute w-full bottom-0 inset-x-0 h-40 bg-gradient-to-b pointer-events-none select-none from-transparent dark:to-black to-white z-40" />
-        <div className="absolute w-full -bottom-20 h-72 md:h-full z-10">
-          <World data={sampleArcs} globeConfig={globeConfig} />;
+      <div className="flex flex-row items-center justify-center py-10 h-screen md:h-auto dark:bg-black bg-white relative w-full">
+        <div className="max-w-7xl mx-auto w-full relative overflow-hidden h-full md:h-[40rem] px-4">
+          <motion.div
+            initial={{
+              opacity: 0,
+              y: 20,
+            }}
+            animate={{
+              opacity: 1,
+              y: 0,
+            }}
+            transition={{
+              duration: 1,
+            }}
+            className="div"
+          >
+            <h2 className="text-center text-xl md:text-4xl font-bold text-black ">
+              E disse-lhes: "Vão pelo mundo todo e preguem o evangelho a todas
+              as pessoas". <br /> Marcos 15:16
+            </h2>
+            <p className="text-center text-base  md:text-lg font-normal text-neutral-700 mt-2 mx-auto">
+              Thanks Aceternity UI for this beautiful component! God bless you
+              all ✨
+            </p>
+          </motion.div>
+          <div className="absolute w-full bottom-0 inset-x-0 h-40 bg-gradient-to-b pointer-events-none select-none from-transparent dark:to-black to-white z-40" />
+          <div className="absolute w-full md:w-full -bottom-20 h-[35rem] md:h-full z-10">
+            <World data={sampleArcs} globeConfig={globeConfig} />;
+          </div>
         </div>
       </div>
-    </div>
   );
 }
