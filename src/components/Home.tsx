d33b1789450819e8937fa9cfbe2f7d244b02669f
jorge luiz <jojoluiz42@gmail.com>
2024-05-25 12:30:07 -0300

    sytle: change text Home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -29,7 +29,7 @@ export default function Home() {
       <h1 className="pt-3 text-center text-3xl">Olá amigos(as)!</h1>
       <p className="text-center pt-3 text-2xl">
         Em Mateus 24:35 o Senhor Jesus diz "-os céus e a terra passarão, mas as minhas palavras jamais passarão." <br />
-        Logo, consegue vê a importância da Palavra? <br />
+        Logo, consegue vê a importância da Palavra? Mas também lembre, o evangelho não é somente em palavras, porém também em poder e ações no Espírito Santo. <br />
         Eu proponho um desafio simples e nada difícil, que é o seguinte: No capítulo que cair em um livro, você lê o capítulo todo. Bem fácil, né? 
         <br /> 
       </p>
