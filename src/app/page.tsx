500474ee87e611ec6f649fc7b3b858a790371e70
jorge luiz <jojoluiz42@gmail.com>
2024-05-27 20:19:38 -0300

    style: importing footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import AboutJesus from "@/components/AboutJesus";
 import CardWordJesus from "@/components/CardWordJesus";
+import Footer from "@/components/Footer";
 import { GlobeDemo } from "@/components/Goble";
 import Home from "@/components/Home";
 import ModalText from "@/components/ModalText";
