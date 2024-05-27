3d8d87b8457e054a3d64b1f4b6ab3586b2197b8c
jorge luiz <jojoluiz42@gmail.com>
2024-05-27 20:17:02 -0300

    feat: Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {};
+
+export default function Footer({}: Props) {
+  return (
+    <div>
+      <footer className="footer footer-center p-4 bg-slate-200 text-base-content">
+        <aside>
+          <p>✝️ Toda glória e honra para o Senhor Jesus</p>
+        </aside>
+      </footer>
+    </div>
+  );
+}
