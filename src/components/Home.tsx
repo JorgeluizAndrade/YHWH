8972a73142c7ab83fa469c6f95abc3a270c17f73
jorge luiz <jojoluiz42@gmail.com>
2024-06-11 18:21:28 -0300

    style: change tex

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -26,12 +26,9 @@ export default function Home() {
           src="https://i.pinimg.com/564x/79/e4/bd/79e4bde615db617869d800c53cf616f1.jpg"
         /> */}
       </div>
-      <h1 className="pt-3 text-center text-3xl">Olá amigos(as)!</h1>
+      <h1 className="pt-3 text-center text-3xl">Marcus 1:15</h1>
       <p className="text-center pt-3 text-lg">
-        Em Mateus 24:35 o Senhor Jesus diz "-os céus e a terra passarão, mas as minhas palavras jamais passarão." <br />
-        Logo, consegue vê a importância da palavra? Mas também lembre, o evangelho não é somente em palavras, porém também em poder e ações no Espírito Santo. <br />
-        Eu proponho um desafio simples e nada difícil, que é o seguinte: No capítulo que cair em um livro, você lê o capítulo todo. Bem fácil, né? 
-        <br /> 
+        O tempo está cumprido, e o Reino de Deus está próximo. Arrependei-vos e crede no evangelho.
       </p>
     </motion.div>
   );
